c663ef9f54410fb43d74f5ee8a0cd63609a156bc
bhargav-umd <32581017+bhargav-umd@users.noreply.github.com>
2017-11-14 17:29:11 -0500

    Add files via upload

--- pixel_to_world.m
+++ pixel_to_world.m
@@ -2,11 +2,26 @@ function markers_cam = pixel_to_world(centroids, KK, Z)
 % Function pixel_to_world converts the pixel co-ordinates to the camera
 % co-ordinates. 
 % It takes in centroids of the three markers as a 3x2 matrix, the camera
-% instrinsic KK and actual distance from the camera to the markers Z and 
+% instrinsic KK and actual  from the camera to the markers Z and 
 % returns the position of the markers in a 3x2 matrix
-
+img= 'markers.jpeg';
+img= imread(img);
+centroids = compute_marker_location(img);
 %%%%%%%%%%%%%%%%%%% YOUR CODE BELOW THIS LINE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+KK =[842.8736 0  327.4053;
+         0  843.8088  219.2094;
+         0         0    1.0000];
+Z=363.3;
 markers_cam = zeros(3,2);
+markers_cam(1,1)= (-KK(1,1)*centroids(1,1))/Z + KK(1,3);
+markers_cam(1,2)= (-KK(2,2)*centroids(1,2))/Z+ KK(2,3);
+
+markers_cam(2,1)= (-KK(1,1)*centroids(2,1))/Z + KK(1,3);
+markers_cam(2,2)= (-KK(2,2)*centroids(2,2))/Z+ KK(2,3);
+
+markers_cam(3,1)= (-KK(1,1)*centroids(3,1))/Z + KK(1,3);
+markers_cam(3,2)= (-KK(2,2)*centroids(3,2))/Z+ KK(2,3);
+
+disp(markers_cam);
+disp(centroids);
 end
