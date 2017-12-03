0a185ecc1aafaf19b57ff26778871302fcfb46b0
ksaimouli <ksaimouli@gmail.com>
2017-12-03 18:18:19 -0500

    input dist

---
+++ FinalCode/mainDemo1.m
@@ -0,0 +1,15 @@
+%% To run the entire project 
+%% Input x,y,z distance in mm
+clockwise = 1;
+anticlockwise = 2;
+
+x= 50; %mm
+dirX= 1;
+
+y= 50; %mm
+dirY= 1;
+
+z= 50; %mm
+dirZ= 1; 
+
+motorMove(x,dirX,y,dirY,z,dirZ);
